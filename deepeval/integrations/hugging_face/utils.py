aaa4bef6ebc235bc70301710bb3c9f1c253da8bc
Darren <99656848+DarrenEmbrace@users.noreply.github.com>
2026-03-26 12:44:02 +0200

    remove torch

--- deepeval/integrations/hugging_face/utils.py
+++ deepeval/integrations/hugging_face/utils.py
@@ -61,7 +61,6 @@ def generate_test_cases(
         decoded_output = tokenizer.decode(outputs[0][tokenized_output["input_ids"].shape[-1]:], skip_special_tokens=True)
         golden.actual_output = decoded_output
         del tokenized_output, outputs
-        torch.cuda.empty_cache()
     test_cases = convert_goldens_to_test_cases(
         goldens=evaluation_dataset.goldens,
         _alias=evaluation_dataset.alias,
